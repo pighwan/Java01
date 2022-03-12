83689043d84eec3d86a467ab7edfab89a9431880
Notebook <Administrator@121.137.222.137>
2022-03-12 23:48:44 +0900

    Java01 복습

--- src/j01_basic/Ex02_Variable02.java
+++ src/j01_basic/Ex02_Variable02.java
@@ -33,27 +33,27 @@ public class Ex02_Variable02 {
 		
 		// 1.4) 기본자료형: char 
 		// => 한글자를 의미, 2byte
-		char cc = 'A' ;
+		char cc = 'A' ; // ""는 한 글자
 		cc='김' ;
 		// String 과 비교
-		String ss = "A" ; 
+		String ss = "A" ; // ""는 문장
 		ss="가나다라" ;
 		
 		// 2) 변수의 사용
 		// => 반드시 선언 후 & 값 할당 후 사용
 		b=127;
 		System.out.println("** byte b => "+b);
-		System.out.println("** boolean bool => "+bool);
-		System.out.println("** char cc => "+cc);
+		System.out.println("** boolean bool => "+bool); // 위의 i가 s보다 크므로 false
+		System.out.println("** char cc => "+cc); // 한 글자 '김'
 		
 		// 3) 최대값 & 최소값 출력하기
 		// => 모든 기본 자료형에는 그 자료형을 지원해 주는 클래스가 있음 : 래퍼(Wrapper)클래스
 		//    해당 클래스명은 기본 자료형의 첫글자를 대문자로 하면됨
-		System.out.println("* byte Max => "+Byte.MAX_VALUE);
+		System.out.println("* byte Max => "+Byte.MAX_VALUE); // byte : -128~0~127
 		System.out.println("* byte Min => "+Byte.MIN_VALUE);
-		System.out.println("* short Max => "+Short.MAX_VALUE);
+		System.out.println("* short Max => "+Short.MAX_VALUE); // short : -32768~0~32767
 		System.out.println("* short Min => "+Short.MIN_VALUE);
-		System.out.println("* int Max => "+Integer.MAX_VALUE);
+		System.out.println("* int Max => "+Integer.MAX_VALUE); // int : -2147483648~0~2147483647
 		System.out.println("* int Min => "+Integer.MIN_VALUE);
 		//System.out.println("* long Max => "+Long.MAX_VALUE);
 		//System.out.println("* long Min => "+Long.MIN_VALUE);
@@ -66,7 +66,7 @@ public class Ex02_Variable02 {
 		
 		// 4) 치환
 		// => 값의 맞교환 
-		//    중간 보관을 위한 같은 타입의 임시 변수가 필요함
+		//    중간 보관을 위한 같은 타입의 임시 변수(temp)가 필요함
 		// => "=" 동일성을 의미하는것이 아니고 대입연산자
 		String cup1 = "콜라" ;
 		String cup2 = "사이다" ;
